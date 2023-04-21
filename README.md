20b840a75aeb33dabd2d1c88cb140e85fbf3fbac
Falk-Witte <krixcrox@duck.com>
2023-04-21 21:14:14 +0200

    no error messages where there shouldn't be any

---
+++ README.md
@@ -0,0 +1,4 @@
+# Why
+Are you sick of always having to remove artefacts from your project folder, so that you don't push a '.idea' folder to your repo? No?
+...
+Well, I do and if you do to, this is the Tool for you!
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::fs::remove_dir_all;
+use std::fs::{remove_dir_all, read_dir};
 
 
 /// Simple cli to remove unwanted files from a git folder
@@ -18,18 +18,29 @@ struct Opts{
 fn main() {
     let args = Opts::parse();
 
-    if args.dir != "" { 
+    if !args.dir.is_empty() { 
         // remove custom user dir
-        remove_dir_all(args.dir).unwrap_or_else(|e| println!("Error: {}", e));
+        remove_dir_all(args.dir).unwrap_or_else(|_| println!("Error: No such directory"));
     }
     
     //TODO: go through all directory entries and check if either .obsidian or .idea exists and remove them via remove_dir_all
     // else don't call remove_dir_all, so there isn't an error message when either dir does not exist in the first place
 
-    // remove obsidian folder
-    remove_dir_all("./.obsidian").unwrap_or_else(|e| println!("Error: {}", e));
+    if let Ok(entries) = read_dir(".") {
+        for entry in entries.flatten(){
+            
+            let entry_string = entry.file_name()
+                                    .into_string()
+                                    .unwrap();  
 
-    // remove idea folder
-    remove_dir_all("./.idea").unwrap_or_else(|e| println!("Error: {}", e));
+            if entry_string == ".obsidian"{
+                // remove obsidian folder
+                remove_dir_all("./.obsidian").unwrap_or_else(|_| println!("Error: No such file or directory"));
+            }else if entry_string == ".idea"{
+                // remove idea folder
+                remove_dir_all("./.idea").unwrap_or_else(|_| println!("Error: No such file or directory"));
+            }
+        }
+    }
     
 }
